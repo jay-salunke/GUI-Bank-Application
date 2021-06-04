64d86e59392eff9959f64fea120c5e462aa5cdb7
Jay-Salunke <jysl2004@gmail.com>
2021-06-04 21:17:15 +0530

    Make Changes in code of Signup Code and half validation and changes are remaining

--- src/JDBCConnection.java
+++ src/JDBCConnection.java
@@ -2,7 +2,7 @@ import java.sql.*;
 public class JDBCConnection {
   protected String Driver="com.mysql.cj.jdbc.Driver";
     protected String DB_URL="jdbc:mysql://localhost:3306/bankapplication";
-    protected String DB_Username="root";
-    protected String DB_pass="Maiboli";
+    protected String DB_Username="username";
+    protected String DB_pass="password";
 }
 
